b0144af28eb58b6e922b8f1cee4d51e48649cfcb
Tochukwu Tobias <jaxj9449@gmail.com>
2025-12-01 01:54:30 +0100

    adjusted the server eslint.config file

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -2,8 +2,8 @@ export default [
   {
     files: ['**/*.js', '**/*.tsx'], //only check .js files
     rules: {
-      semi: 'error', //enforce semicolons at the end of statements
-      'no-unused-vars': 'warn', //warn about variables that are declared but not used
+      semi: 'off', //enforce semicolons at the end of statements
+      'no-unused-vars': 'off', //warn about variables that are declared but not used
       'no-console': 'off', //allow console statements
     },
   },
